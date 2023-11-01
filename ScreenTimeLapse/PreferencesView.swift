2ea5648bc50e84763224102b6efcf294019de8de
William Kaiser <wkaisertexas@gmail.com>
2023-11-01 13:31:32 -0400

    Basic PreferencesView() setup

---
+++ ScreenTimeLapse/PreferencesView.swift
@@ -0,0 +1,20 @@
+//
+//  PreferencesView.swift
+//  ScreenTimeLapse
+//
+//  Created by William Kaiser on 10/31/23.
+//
+
+import SwiftUI
+
+struct PreferencesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PreferencesView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferencesView()
+    }
+}
