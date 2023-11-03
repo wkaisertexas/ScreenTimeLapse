36290b1821c34e682743a7c8ef537b0f462e2676
William Kaiser <wkaisertexas@gmail.com>
2023-11-03 16:20:29 -0400

    Assets

--- ScreenTimeLapseUITests/ScreenTimeLapseUITests.swift
+++ ScreenTimeLapseUITests/ScreenTimeLapseUITests.swift
@@ -8,27 +8,35 @@
 
 import XCTest
 
+
+
 final class ScreenTimeLapseUITests: XCTestCase {
+    let app = XCUIApplication()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testRecording() throws {
+        
         let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let mainWindwo = app.windows["recorder.main"]
+        
+        
+        
+        app.menuBars.containing(.menuBarItem, identifier:"Apple").element.click()
+        
     }
 
     func testLaunchPerformance() throws {
