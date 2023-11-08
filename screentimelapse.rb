a127f0542e5152f90c02e436c033a09e937b9c1f
William Kaiser <wkaisertexas@gmail.com>
2023-11-08 14:33:07 -0500

    Added cask

---
+++ screentimelapse.rb
@@ -0,0 +1,15 @@
+cask "screentimelapse" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://github.com/wkaisertexas/ScreenTimeLapse/releases/download/v1.0.0/ScreenTimeLapse.app.zip"
+  name "ScreenTimeLapse"
+  desc "Record screen time lapses with ease in a simple, intuitive interface"
+  homepage "https://github.com/wkaisertexas/ScreenTimeLapse"
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "ScreenTimeLapse.app"
+end
+
