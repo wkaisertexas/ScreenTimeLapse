eb15c06bb503d6665bd6a188883d1aec85480370
William Kaiser <wkaisertexas@gmail.com>
2023-11-12 12:08:03 -0500

    Update screentimelapse.rb

--- screentimelapse.rb
+++ screentimelapse.rb
@@ -2,7 +2,7 @@ cask "screentimelapse" do
   version "1.0.0"
   sha256 :no_check
 
-  url "https://github.com/wkaisertexas/ScreenTimeLapse/releases/download/v1.0.0/ScreenTimeLapse.app.zip"
+  url "https://github.com/wkaisertexas/ScreenTimeLapse/releases/download/v1.0.0/ScreenTimeLapse.zip"
   name "ScreenTimeLapse"
   desc "Record screen time lapses with ease in a simple, intuitive interface"
   homepage "https://github.com/wkaisertexas/ScreenTimeLapse"
