3e096f41c3bff12f2b6fed1f96540f3c1ce9c165
William Kaiser <wkaisertexas@gmail.com>
2024-12-04 22:04:19 -0500

    Update timelapze.rb

--- timelapze.rb
+++ timelapze.rb
@@ -1,14 +1,14 @@
 cask "timelapze" do
-  version "1.0.0"
+  version "1.2.2"
   sha256 :no_check
 
-  url "https://github.com/wkaisertexas/ScreenTimeLapse/releases/download/v1.0.0/TimeLapze.zip"
+  url "https://github.com/wkaisertexas/ScreenTimeLapse/releases/download/v1.2.2/TimeLapze.zip"
   name "TimeLapze"
   desc "Record screen time lapses with ease in a simple, intuitive interface"
   homepage "https://github.com/wkaisertexas/ScreenTimeLapse"
 
   auto_updates true
-  depends_on macos: ">= :ventura"
+  depends_on macos: ">= :Sonoma"
 
   app "TimeLapze.app"
 end
